2df13e97459d48f7e6a1c350f967ea55595f9344
Phúc Thịnh <phucthinh0342@gmail.com>
2021-09-16 17:35:42 +0700

    a

---
+++ Bai01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Bai01{
+     
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String message;
+    System.out.println("\n\nNhap vao chuoi can in hoa: ");
+    message = sc.nextLine();
+    System.out.println("\n\nChuoi in hoa la: ");
+    System.out.println(message.toUpperCase());
+ 
+}
+}
---
+++ Bai02.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Bai02{
+     
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+
+        System.out.print("Nhập vào số nguyên n = ");
+        int n=sc.nextInt();
+
+        if(isPrime(n)){
+            System.out.println(n+" là số nguyên tố!");
+        } else {
+            System.out.println(n+" không là số nguyên tố!");
+        }
+    }
+    
+}
+
