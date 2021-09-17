e9bd7874f595183ee001ccf7b99590bb39771e9f
Phúc Thịnh <phucthinh0342@gmail.com>
2021-09-17 19:05:00 +0700

    a

--- PhanSo.java
+++ PhanSo.java
@@ -1,8 +1,8 @@
 public class PhanSo {
-    float tuSo;
-    float mauSo;
+    int tuSo;
+    int mauSo;
 
-    PhanSo(float TuSo, float MauSo){
+    PhanSo(int TuSo, int MauSo){
         tuSo = TuSo;
         mauSo = MauSo;
     }
@@ -16,17 +16,39 @@ public class PhanSo {
     }
 
    
+    void nghichdao(){
+        System.out.println("Nghịch đảo phân số: "+(int)mauSo+"/"+(int)tuSo);
+    }
 
-  
+    static void tinhtoanphanso(PhanSo a, PhanSo b){
+       int ts1, ts2, msc;
+
+       ts1 = a.tuSo*b.mauSo;
+       ts2 = b.tuSo*a.mauSo;
+       msc = a.mauSo*b.mauSo;
+
+       System.out.println("Cộng 2 phân số: "+(ts1+ts2)+"/"+msc);
+       System.out.println("Trừ 2 phân số: "+(ts1-ts2)+"/"+msc);
+       System.out.println("Nhân 2 phân số: "+(a.tuSo*b.tuSo)+"/"+(a.mauSo*b.mauSo));
+       System.out.println("Chia 2 phân số: "+(a.tuSo*b.mauSo)+"/"+(a.mauSo*b.tuSo));
+    }
 
     public static void main(String[] args) {
         PhanSo p1 = new PhanSo(4, 8);
         p1.inPhanSo();
         p1.sothuc();
+        p1.nghichdao();
+        
+        System.out.println("...................................................");
 
         PhanSo p2 =  new PhanSo(3, 7);
         p2.inPhanSo();
         p2.sothuc();
+        p2.nghichdao();
+
+        System.out.println("......................................................");
+
+        tinhtoanphanso(p1, p2);
         
     }
 }
---
+++ Point.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Point {
+    int x;
+    int y;
+
+    void toado(int X, int Y){
+        this.x = X;
+        this.y = Y;
+    }
+
+    void inpoint(){
+        System.out.println("Tọa Độ (X,Y): ("+x+","+y+")");
+    }
+
+    void inputp(){
+        Scanner sc = new Scanner(System.in);
+        int a , b;   
+        
+        System.out.println("Nhập tọa độ X, Y: ");
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        toado(a, b);
+        
+    }
+
+    void tinhkhoangcach(){
+        double td;
+
+        td = Math.sqrt((x*x)+(y*y));
+
+        System.out.println("Khoang cach voi toa do (0,0): "+td);
+    }
+
+    public static void main(String[] args) {
+
+        Point p1 = new Point();
+        p1.toado(3, 5);
+        p1.inpoint();
+        p1.tinhkhoangcach();
+
+        System.out.println("-------------------------------------------");
+        
+        Point p2 = new Point();
+        p2.inputp();
+        p2.inpoint();
+        p2.tinhkhoangcach();
+    
+    }
+}
