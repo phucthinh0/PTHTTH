076495b205a7a4112b3b3e264e26363d7daf5b44
Phúc Thịnh <phucthinh0342@gmail.com>
2021-11-06 14:33:36 +0700

    a

--- Interface.java
+++ Interface.java
@@ -2,7 +2,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Interface extends Remote {
-    public String dao(String[] s) throws RemoteException;
+   
 
     public String inhoa(String[] s) throws RemoteException;
 
